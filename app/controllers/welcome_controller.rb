2ef28d0442cdb925b17df1033c2940d32257cf8a
Matt Watson <Matt.Watson@orasi.com>
2014-11-07 08:36:22 -0500

    do not create blank users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,20 +5,27 @@ class WelcomeController < ApplicationController
   end
 
   def validate
-    params[:user][:username].downcase!
-    @user = User.find_by(username: params[:user][:username])
-    respond_to do |format|
-      if @user.nil?
-        @user = User.create_user(params[:user][:username], params[:user][:password])
-        format.html { redirect_to user_path(@user.id) }
-        format.json { render json: @user }
-      elsif @user.password == params[:user][:password]
-        format.html { redirect_to user_path(@user.id) }
-        format.json { render json: @user }
-      else
-        format.html { redirect_to :root }
-        format.json { render json: nil }
+     
+     format.json { render json: nil} if params[:user][:username].blank? || params[:user][:password].blank?
+     
+    begin
+      params[:user][:username].downcase!
+      @user = User.find_by(username: params[:user][:username])
+      respond_to do |format|
+        if @user.nil?
+	  @user = User.create_user(params[:user][:username], params[:user][:password])
+    	  format.html { redirect_to user_path(@user.id) }
+ 	  format.json { render json: @user }
+        elsif @user.password == params[:user][:password]
+  	  format.html { redirect_to user_path(@user.id) }
+	  format.json { render json: @user }
+        else
+	  format.html { redirect_to :root }
+	  format.json { render json: nil }
+        end
       end
+    rescue
+      format.json { render json: nil}
     end
   end
 end
